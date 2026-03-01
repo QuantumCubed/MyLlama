f545bad8a9b5485f1a803f2332c9dd219b0e723a
Anish Kurani <anish.kurani09@gmail.com>
2026-03-01 04:25:33 -0500

    added HA WebSocket connection support.

--- lib/tools/Tool_Functions.py
+++ lib/tools/Tool_Functions.py
@@ -1,5 +1,24 @@
-import lib.integrations.IoT as IOT
+from lib.integrations.IoT import HomeAssistantWebSocket
 
-def time_now() -> str:
-    """Get the local time to the user"""
-    return "The time is 2:20"
+class ExternalTools:
+    def __init__(self, HA: HomeAssistantWebSocket) -> None:
+        self.HA = HA
+
+    def time_now(self) -> str:
+        """Get the local time to the user"""
+        return "The time is 2:20"
+
+    async def standup_lamp_toggle(self) -> str:
+        """Toggle the standup lamp"""
+        message = {
+            "type": "call_service",
+            "domain": "switch",
+            "service": "toggle",
+            "service_data": {
+                "entity_id": "switch.standup_lamp"
+            }
+        }
+
+        response = await self.HA.send(message)
+
+        return f"success: {response["success"]}"
