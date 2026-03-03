0ec578d6cac35efa91352299a29d2eabc21929dc
Anish Kurani <anish.kurani09@gmail.com>
2026-03-02 22:39:38 -0500

    basic langchain router setup

--- lib/chat/LLM_Operations.py
+++ lib/chat/LLM_Operations.py
@@ -4,6 +4,7 @@ from lib.tools.Tool_Functions import ExternalTools
 from lib.tools import Tool_Schema
 from typing import AsyncGenerator
 import inspect
+import json
 class LLM_OPS:
     def __init__(self) -> None:
         try:
@@ -12,8 +13,13 @@ class LLM_OPS:
             self.tools = {}
             self.definitions
             self.conversation = []
+            self.router_instructions = [
+                { "role": "system", "content": "You are a router that determines if a user's query requires extra functions to respond." },
+                { "role": "system", "content": "Determine if the prompt requires a tool call. Available tools include: smart home management and time tools." },
+                { "role": "system", "content": "Determine if the prompt requires the model to think. The model should only think if the user's query is very complex or requires in-depth reasoning" },
+                { "role": "system", "content": "Return the requirements in the following format: {tool_call: True/False, think_required: True/False}" },
+            ]
             print("CONNECTION ESTABLISHED, OLLAMA CLIENT INITIALIZED!")
-            
         except:
             print("ERROR INITIALIZING OLLAMA CLIENT!")
 
@@ -29,11 +35,86 @@ class LLM_OPS:
             for method in self.tools.values()
         ]
     
-    async def chat(self, prompt: str) -> AsyncGenerator[str, None]:
+    async def route(self, prompt: str) -> tuple[bool | None, AsyncGenerator[str, None] | str | None]:
+
+        response: ChatResponse = await self.OllamaClient.chat(
+            model="qwen3:14b",
+            messages=self.router_instructions + [{ "role": "user", "content": prompt }],
+            tools=None,
+            think=False,
+            stream=False
+        )
+
+        if not response.message.content:
+            print("SOME ERROR OCCURED!")
+            return None, None
+
+        router_dict: dict = json.loads(response.message.content)
+
+        print(router_dict)
+
+        tool_call: bool = router_dict.get("tool_call") or False # if tool call is True then stream is False; otherwise stream is always True i.e. tool_call is always False
+        think: bool = router_dict.get("think_required") or False
+
+        return (not tool_call, (await self.chat(prompt, think) if tool_call else self.chat_stream(prompt, think)))
+    
+    async def chat(self, prompt: str, think: bool) -> str | None:
 
         self.conversation.append({
             "role": "user",
             "content": prompt
+        })
+
+        while True:
+            response: ChatResponse = await self.OllamaClient.chat(
+                model="qwen3:14b",
+                messages=self.conversation,
+                tools=self.definitions,
+                think=think,
+                stream=False,
+            )
+
+            if not response.message.tool_calls:
+
+                self.conversation.append({
+                    "role": "assistant", # change "assistant" to whatever the model name/role is
+                    "content": response.message.content
+                })
+
+                print(response.message.thinking)
+
+                # add sliding window or someother context window management solution - eventually RAG
+
+                print(self.conversation)
+
+                return response.message.content
+
+            self.conversation.append(response.message)
+
+            for tool_call in response.message.tool_calls:
+                tool = self.tools.get(tool_call.function.name)
+
+                if tool is None:
+                    result = f"Unknown tool: {tool_call.function.name}"
+                else:
+                    if inspect.iscoroutinefunction(tool):
+                        result = await tool(**tool_call.function.arguments)
+                    else:
+                        result = tool(**tool_call.function.arguments)
+                    
+
+                self.conversation.append({
+                    "role": "tool",
+                    "tool_name": tool_call.function.name,
+                    "content": result
+                })
+
+    async def chat_stream(self, prompt: str, think: bool) -> AsyncGenerator[str, None]:
+
+        await self.route(prompt) # this is temporary
+
+        self.conversation.append({
+            "role": "user", "content": prompt
         })
 
         completed_response = ""
@@ -41,8 +122,8 @@ class LLM_OPS:
         async for chunk in await self.OllamaClient.chat(
             model="qwen3:14b",
             messages=self.conversation,
-            tools=self.definitions,
-            think=False,
+            tools=None,
+            think=think,
             stream=True,
         ):
             content = chunk.message.content
@@ -56,55 +137,3 @@ class LLM_OPS:
         })
 
         print(self.conversation)
-
-    # async def chat(self, prompt: str) -> str | None:
-
-    #     self.conversation.append({
-    #         "role": "user",
-    #         "content": prompt
-    #     })
-
-    #     while True:
-    #         response: ChatResponse = await self.OllamaClient.chat(
-    #             model="qwen3:14b",
-    #             messages=self.conversation,
-    #             # tools=[Tool_Functions.time_now],
-    #             tools=self.definitions,
-    #             think=False,
-    #             stream=False,
-    #         )
-
-    #         if not response.message.tool_calls:
-
-    #             self.conversation.append({
-    #                 "role": "assistant", # change "assistant" to whatever the model name/role is
-    #                 "content": response.message.content
-    #             })
-
-    #             print(response.message.thinking)
-
-    #             # add sliding window or someother context window management solution - eventually RAG
-
-    #             print(self.conversation)
-
-    #             return response.message.content
-
-    #         self.conversation.append(response.message)
-
-    #         for tool_call in response.message.tool_calls:
-    #             tool = self.tools.get(tool_call.function.name)
-
-    #             if tool is None:
-    #                 result = f"Unknown tool: {tool_call.function.name}"
-    #             else:
-    #                 if inspect.iscoroutinefunction(tool):
-    #                     result = await tool(**tool_call.function.arguments)
-    #                 else:
-    #                     result = tool(**tool_call.function.arguments)
-                    
-
-    #             self.conversation.append({
-    #                 "role": "tool",
-    #                 "tool_name": tool_call.function.name,
-    #                 "content": result
-    #             })
